79e99d48af08b55d30f9574b4fc33af8bfcc8924
Charlie Fyvie-Gauld <cfg@zunautica.org>
2017-01-09 17:21:26 +0000

    Fix provider names

--- app/Providers/BulletinRepositoryProvider.php
+++ app/Providers/BulletinRepositoryProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use SpringDvs\Core\NetServices\BulletinRepository as BulletinRepository;
+use App\Models\BulletinRepositoryModel as BulletinRepositoryModel;
 
 class BulletinRepositoryProvider extends ServiceProvider
 {
@@ -15,13 +15,13 @@ class BulletinRepositoryProvider extends ServiceProvider
      */
     public function register()
     {
-		$this->app->bind('App\Providers\BulletinRepositoryModel', function($app){
-			return new BulletinRepositoryModel($app->make('db.connection'), $app->make('App\Models\LocalNodeModel'));
+		$this->app->bind(\App\Models\BulletinRepositoryModel::class, function($app){
+			return new BulletinRepositoryModel($app->make('db.connection'), $app->make(App\Models\LocalNodeModel::class));
 		});
 		
 		$this->app->bind(
-			'SpringDvs\Core\NetServices\BulletinRepositoryInterface',
-			'App\Models\BulletinRepositoryModel'
+			\SpringDvs\Core\NetServices\BulletinRepositoryInterface::class,
+			\App\Models\BulletinRepositoryModel::class
 		);
     }
 }
